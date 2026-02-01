1dadb2af6200711bc3dace86ceb8734b90d8bdc8
shekharpeddi <shekhar.peddi@gmail.com>
2026-02-01 23:40:57 +0530

    aws-cli

--- tf_piyush/ins.sh
+++ tf_piyush/ins.sh
@@ -2,3 +2,9 @@
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
+#aws cli 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#install unzip
+ apt install unzip
+ unzip awscliv2.zip
+sudo ./aws/install
