bba9387cf1ecc1d878d7fc4166dc2290a05ab646
shekharpeddi <shekhar.peddi@gmail.com>
2026-02-01 23:14:29 +0530

    dddd

--- tf_piyush/main.tf
+++ tf_piyush/main.tf
@@ -9,7 +9,6 @@ terraform {
 }
 
 
-
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
@@ -27,6 +26,32 @@ resource "aws_vpc" "example" {
   }
 }
 
+data "aws_region" "current" {}
+
+resource "aws_vpc_ipam" "test" {
+  operating_regions {
+    region_name = data.aws_region.current.region
+  }
+}
+
+resource "aws_vpc_ipam_pool" "test" {
+  address_family = "ipv4"
+  ipam_scope_id  = aws_vpc_ipam.test.private_default_scope_id
+  locale         = data.aws_region.current.region
+}
+
+resource "aws_vpc_ipam_pool_cidr" "test" {
+  ipam_pool_id = aws_vpc_ipam_pool.test.id
+  cidr         = "172.20.0.0/16"
+}
+
+resource "aws_vpc" "test" {
+  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.test.id
+  ipv4_netmask_length = 28
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.test
+  ]
+}
 #create s3 bucket
  resource "aws_s3_bucket" "demo" {
    bucket = "my-tf-test-shekar-bucket-101"
