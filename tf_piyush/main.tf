bf313ca2f5c1c26d7eabb45178ff982588c4ddf3
shekharpeddi <shekhar.peddi@gmail.com>
2026-02-02 10:58:17 +0530

    today

--- tf_piyush/main.tf
+++ tf_piyush/main.tf
@@ -8,6 +8,22 @@ terraform {
   }
 }
 
+#create ami ubuntu
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = [var.instance_type[0]]
+
+#instance count
+count = var.instance_count
+
+#Bool type
+monitoring = var.enable_monitoring
+associate_public_ip_address = var.associate_public_ip_address
+ 
+ 
+  tags = {
+    Name = "${var.environment}-ec2-tf-instance"  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
@@ -26,32 +42,7 @@ resource "aws_vpc" "example" {
   }
 }
 
-data "aws_region" "current" {}
 
-resource "aws_vpc_ipam" "test" {
-  operating_regions {
-    region_name = data.aws_region.current.region
-  }
-}
-
-resource "aws_vpc_ipam_pool" "test" {
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.test.private_default_scope_id
-  locale         = data.aws_region.current.region
-}
-
-resource "aws_vpc_ipam_pool_cidr" "test" {
-  ipam_pool_id = aws_vpc_ipam_pool.test.id
-  cidr         = "172.20.0.0/16"
-}
-
-resource "aws_vpc" "test" {
-  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.test.id
-  ipv4_netmask_length = 28
-  depends_on = [
-    aws_vpc_ipam_pool_cidr.test
-  ]
-}
 #create s3 bucket
  resource "aws_s3_bucket" "demo" {
    bucket = "my-tf-test-shekar-bucket-101"
@@ -62,11 +53,3 @@ resource "aws_vpc" "test" {
    }
 } 
 
-#create ami ubuntu
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "${var.environment}-ec2-tf-instance"  }
-}
--- tf_piyush/variables.tf
+++ tf_piyush/variables.tf
@@ -1,12 +1,38 @@
 variable "environment" {
-   description = "Environment Name"
+   description = "environmet type"
    type = string
    default = "Dev"
 }
 
 variable "region" {
     description = "aws region"
-    type = string
-    default = "us-east-1"
+    type = list(string)
+    default = ["us-east-1" ,"us-east-2","us-west-1"]
+      
+}
+
+variable "instance_type" {
+    description = "type of disk"
+    type = list(string)
+    default = [ "t2.micro","t2.small","t2.medium" ]
+}
+
+#Number type
+variable "instance_count" {
+    type = number
+    description = "no of ec2 instance to create"
+    default = 1
+}
+
+#Bool type
+variable "enable_monitoring" {
+    type = bool
+    description = "enable monitoring for ec2 instance"
+    default = true
   
+}
+variable "associate_public_ip_address" {
+   type = bool
+   description = "associate public address"
+   default = true
 }
